b5ceb8f78bd41a5f3db09c6d79dcef5583d67181
rv-raunak <rvr.vaibhavmishra@gmail.com>
2026-07-28 13:40:00 +0530

    feat(backend): initialize spring boot main application entry point

---
+++ project-aarambh-backend/project-aarambh/src/main/java/com/springBuster/project_aarambh/ProjectAarambhApplication.java
@@ -0,0 +1,13 @@
+package com.springBuster.project_aarambh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAarambhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAarambhApplication.class, args);
+	}
+
+}
