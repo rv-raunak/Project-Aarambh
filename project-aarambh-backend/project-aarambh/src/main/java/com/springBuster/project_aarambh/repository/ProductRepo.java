0dbf6cc9e4b72ef3c3304518100bcc33ceb4527a
rv-raunak <rvr.vaibhavmishra@gmail.com>
2026-07-30 11:25:00 +0530

    feat(backend): implement ProductRepo interface with custom jpql search queries

---
+++ project-aarambh-backend/project-aarambh/src/main/java/com/springBuster/project_aarambh/repository/ProductRepo.java
@@ -0,0 +1,24 @@
+package com.springBuster.project_aarambh.repository;
+
+import com.springBuster.project_aarambh.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+
+    // custom queries using JPQL (Jakarta Persistence Query language)
+    @Query("SELECT p from  Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword,'%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword,'%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword,'%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword,'%')) "
+    )
+    List<Product> searchProducts(String keyword);
+
+    // here p is alias for product which is the name of the class and not the entity/table
+}
+
