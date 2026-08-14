0170813d95221e3267e1ae739363b58f055a8845
rv-raunak <rvr.vaibhavmishra@gmail.com>
2026-08-14 10:15:00 +0530

    test(backend): add initial context load test

---
+++ project-aarambh-backend/project-aarambh/src/test/java/com/springBuster/project_aarambh/ProjectAarambhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBuster.project_aarambh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAarambhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
